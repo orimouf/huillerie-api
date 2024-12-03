e8f2c334c7ab237102641f6d030c00460c011ac5
Amir Harrouche <orim.ouf@gmail.com>
2024-12-03 11:05:55 +0100

    update 03-12-2024

--- routes/clients.js
+++ routes/clients.js
@@ -79,7 +79,7 @@ router.get("/find/:id", async (req, res) => {
 router.get("/", async (req, res) => {
     // if(req.user.isAdmin) {
         try {
-            const clients = Client.find()
+            const clients = await Client.find()
             res.status(200).json({ clients })
         } catch (err) {
             res.status(500).json(err)
