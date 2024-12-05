009845746a2b2c25dbfcfdf4fb642c6fc7eef95e
Amir Harrouche <orim.ouf@gmail.com>
2024-12-05 17:41:49 +0100

    update 05-12-2024

--- models/UniqueId.js
+++ models/UniqueId.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose")
 
 const UniqueIdSchema = new mongoose.Schema(
     {
-        appId: { type: String, required: true, unique: false },
-        clientServerId: { type: String, required: true, unique: true },
-        UniqueId: { type: String, required: true, unique: true }
+        idName: { type: String, required: true, unique: true },
+        uniqueId: { type: Number, required: true, unique: true }
     },
     {timestamps: true}
 )
