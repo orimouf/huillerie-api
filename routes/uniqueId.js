7be12ec081c44e759aa39df138ec1084c0aef2df
Amir Harrouche <orim.ouf@gmail.com>
2024-12-05 17:50:13 +0100

    update 05-12-2024

--- routes/uniqueId.js
+++ routes/uniqueId.js
@@ -5,29 +5,21 @@ const verify = require("../verifyToken")
 
 //UPDATE
 
-// router.put("/:id", verify, async (req, res) => {
-//     if(req.user.id === req.params.id || req.user.isAdmin) {
-//         if(req.body.password){
-//             req.body.password = CryptoJS.AES.encrypt(
-//                     req.body.password,
-//                     process.env.SECRET_KEY
-//                 ).toString()
-//         }
-//         try {
-//             const updatedUniqueId = await UniqueId.findByIdAndUpdate(req.params.id, 
-//                 {
-//                     $set:req.body,
-//                 },
-//                 { new: true }
-//             )
-//             res.status(200).json(updatedUniqueId)
-//         } catch (err) {
-//             res.status(500).json(err)
-//         }
-//     } else {
-//         res.status(500).json("you can update only your account!")
-//     }
-// })
+router.put("/", async (req, res) => {
+
+        try {
+            const updatedUniqueId = await UniqueId.findOneAndUpdate({idName: "clientAutoval"}, 
+                {
+                    "$inc": {"uniqueId": 1}
+                    // $set:req.body,
+                },
+                { new: true }
+            )
+            res.status(200).json(updatedUniqueId)
+        } catch (err) {
+            res.status(500).json(err)
+        }
+})
 
 //DELETE
 
